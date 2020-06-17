2dd03710cf23249240d9baa3f3460ff139bf6588
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-17 00:15:01 -0300

    it's working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 export default class App extends Component {
   render() {
     return (
-      <div className={`${'card-panel #e8eaf6 indigo lighten-5'} ${'row'} `}>
+      <div className={`${'card-panel #e8eaf6 indigo lighten-5'} `}>
         <h4 style={{ textAlign: 'center' }}>Cálculo do Salário Líquido</h4>
         <Header />
       </div>
