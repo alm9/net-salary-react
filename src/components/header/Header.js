aa90d3d8bc7e09186037c3217065de9e7848ea22
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-23 15:13:36 -0300

    using Hooks instead of Class component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,30 +3,38 @@
  * by: André de Lima Machado
  */
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GrossSalary from './fullSalary/GrossSalary';
 import { calculateSalaryFrom } from '../calculateSalary/salary';
 import ReadOnly from './readOnly/ReadOnly';
 import AllBars from './bars/AllBars';
 
-export default class ProjetoBase extends Component {
-  constructor() {
-    super();
+// export default class ProjetoBase extends Component {
+export default function ProjetoBase() {
+  const [grossSal, setGrossSal] = useState('');
+  const [baseINSS, setBaseINSS] = useState(0);
+  const [discountINSS, setDiscountINSS] = useState(0);
+  const [percentageINSS, setPercentageINSS] = useState(0);
+  const [baseIRPF, setBaseIRPF] = useState(0);
+  const [discountIRPF, setDiscountIRPF] = useState(0);
+  const [percentageIRPF, setPercentageIRPF] = useState(0);
+  const [netSal, setNetSal] = useState(0);
 
-    this.state = {
-      grossSal: '',
-      baseINSS: 0,
-      discountINSS: 0,
-      percentageINSS: 0,
-      baseIRPF: 0,
-      discountIRPF: 0,
-      percentageIRPF: 0,
-      netSal: 111,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     grossSal: '',
+  //     baseINSS: 0,
+  //     discountINSS: 0,
+  //     percentageINSS: 0,
+  //     baseIRPF: 0,
+  //     discountIRPF: 0,
+  //     percentageIRPF: 0,
+  //     netSal: 0,
+  //   };
+  // }
 
-  handleChangeGrossSalary = (newValueGrossSal) => {
-    // console.log(valueGrossSal);
+  const handleChangeGrossSalary = (newValueGrossSal) => {
     const {
       baseINSS,
       discountINSS,
@@ -37,83 +45,78 @@ export default class ProjetoBase extends Component {
       netSalary,
     } = calculateSalaryFrom(newValueGrossSal);
 
-    // console.log(discountIRPF);
+    setGrossSal(newValueGrossSal);
+    setBaseINSS(baseINSS);
+    setBaseIRPF(baseIRPF);
+    setDiscountINSS(discountINSS);
+    setDiscountIRPF(discountIRPF);
+    setNetSal(netSalary);
+    setPercentageINSS(percentageINSS);
+    setPercentageIRPF(percentageIRPF);
 
-    this.setState({
-      grossSal: newValueGrossSal,
-      baseINSS,
-      discountINSS,
-      percentageINSS,
-      baseIRPF,
-      discountIRPF,
-      percentageIRPF,
-      netSal: netSalary,
-    });
+    //   this.setState({
+    //     grossSal: newValueGrossSal,
+    //     baseINSS,
+    //     discountINSS,
+    //     percentageINSS,
+    //     baseIRPF,
+    //     discountIRPF,
+    //     percentageIRPF,
+    //     netSal: netSalary,
+    //   });
   };
 
-  render() {
-    const {
-      grossSal,
-      baseINSS,
-      discountINSS,
-      percentageINSS,
-      baseIRPF,
-      discountIRPF,
-      percentageIRPF,
-      netSal,
-    } = this.state;
+  // render() {
+  // const {
+  //   grossSal,
+  //   baseINSS,
+  //   discountINSS,
+  //   percentageINSS,
+  //   baseIRPF,
+  //   discountIRPF,
+  //   percentageIRPF,
+  //   netSal,
+  // } = this.state;
 
-    return (
-      <div>
-        {/* input: */}
-        <GrossSalary
-          grossSalary={grossSal}
-          onChangeGrossSalary={this.handleChangeGrossSalary}
-        />
-        {/* outputs: */}
-        <div className="row">
-          <div className="col s3">
-            <ReadOnly
-              className="col s3"
-              result={baseINSS}
-              label={'Base INSS:'}
-            />
-          </div>
-          <div className="col s3">
-            <ReadOnly
-              className="col s3"
-              result={discountINSS}
-              percentage={percentageINSS}
-              label={'Desconto INSS:'}
-            />
-          </div>
-          <div className="col s3">
-            <ReadOnly
-              className="col s3"
-              result={baseIRPF}
-              label={'Base IRPF:'}
-            />
-          </div>
-          <div className="col s3">
-            <ReadOnly
-              className="col s3"
-              result={discountIRPF}
-              percentage={percentageIRPF}
-              label={'Desconto IRPF:'}
-            />
-          </div>
+  return (
+    <div>
+      {/* input: */}
+      <GrossSalary
+        grossSalary={grossSal}
+        onChangeGrossSalary={handleChangeGrossSalary}
+      />
+      {/* outputs: */}
+      <div className="row">
+        <div className="col s3">
+          <ReadOnly className="col s3" result={baseINSS} label={'Base INSS:'} />
         </div>
-        <ReadOnly
-          result={netSal}
-          grossSalary={grossSal}
-          label={'Salário Líquido:'}
-        />
-        <AllBars
-          barINSS={discountINSS}
-          barIRPF={discountIRPF}
-          barLiq={netSal}
-        />
+        <div className="col s3">
+          <ReadOnly
+            className="col s3"
+            result={discountINSS}
+            percentage={percentageINSS}
+            label={'Desconto INSS:'}
+          />
+        </div>
+        <div className="col s3">
+          <ReadOnly className="col s3" result={baseIRPF} label={'Base IRPF:'} />
+        </div>
+        <div className="col s3">
+          <ReadOnly
+            className="col s3"
+            result={discountIRPF}
+            percentage={percentageIRPF}
+            label={'Desconto IRPF:'}
+          />
+        </div>
       </div>
-    );
-  }
+      <ReadOnly
+        result={netSal}
+        grossSalary={grossSal}
+        label={'Salário Líquido:'}
+      />
+      <AllBars barINSS={discountINSS} barIRPF={discountIRPF} barLiq={netSal} />
+    </div>
+  );
+  // }
 }
